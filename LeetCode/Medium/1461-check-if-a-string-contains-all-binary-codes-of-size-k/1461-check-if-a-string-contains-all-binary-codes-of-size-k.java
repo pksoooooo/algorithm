5ffc1db44a58299a96e8bcf1b08a74238282ea04
Kwonsoo Park <45193994+pksoooooo@users.noreply.github.com>
2025-04-15 14:38:52 +0900

    Time: 131 ms (73.36%), Space: 68.6 MB (51.47%) - LeetHub

--- LeetCode/Medium/1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.java
+++ LeetCode/Medium/1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.java
@@ -3,15 +3,13 @@ class Solution {
 
         int bitTotal = 1 << k;
         Set<String> set = new HashSet<>();
-            
-            for(int i = 0; i <= s.length() - k; i++){
-                set.add(s.substring(i, i + k));
-                if(set.size() == bitTotal){
-                    return true;
-                }
-                
+        for(int i = 0; i <= s.length() - k; i++){
+            set.add(s.substring(i, i+k));
+            if(set.size() == bitTotal){
+                return true;
             }
-
+            
+        }
 
         return false;
         
