26b0bc6d6903c0b0dd3dd09677d7785e5f7620f0
Kwonsoo Park <45193994+pksoooooo@users.noreply.github.com>
2025-04-18 10:03:18 +0900

    [Silver V] Title: 학생 인기도 측정, Time: 168 ms, Memory: 15228 KB -BaekjoonHub

--- 백준/Silver/25325. 학생 인기도 측정/학생 인기도 측정.java
+++ 백준/Silver/25325. 학생 인기도 측정/학생 인기도 측정.java
@@ -1,39 +1,48 @@
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
         int n = Integer.parseInt(sc.nextLine());
+        HashMap<String, Integer> students = new HashMap<>();
 
-        // 학생 이름들 순서대로 저장
         String[] names = sc.nextLine().split(" ");
-
-        // 인기도 저장용 map
-        Map<String, Integer> popularity = new HashMap<>();
         for (String name : names) {
-            popularity.put(name, 0);
+            students.put(name, 0);
         }
 
-        // 각 학생이 좋아하는 사람 정보 받아서 처리
         for (int i = 0; i < n; i++) {
-            String[] liked = sc.nextLine().split(" ");
-            for (String target : liked) {
-                popularity.put(target, popularity.get(target) + 1);
+            String[] student = sc.nextLine().split(" ");
+            for (String str : student) {
+                if (!str.equals(names[i])) {
+                    students.put(str, students.get(str) + 1);
+                }
+
             }
+
         }
 
-        // 정렬: 인기도 내림차순, 같으면 이름 오름차순
-        Arrays.sort(names, (a, b) -> {
-            int cmp = Integer.compare(popularity.get(b), popularity.get(a));
-            if (cmp == 0) {
-                return a.compareTo(b);
+        ArrayList<String> list = new ArrayList<>(students.keySet());
+
+        list.sort(new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                int cmp = students.get(s2).compareTo(students.get(s1));
+                if (cmp != 0) {
+                    return cmp;
+                }
+                return s1.compareTo(s2);
+
             }
-            return cmp;
         });
 
-        for (String name : names) {
-            System.out.println(name + " " + popularity.get(name));
+        for (String key : list) {
+            System.out.println(key + " " + students.get(key));
         }
+
     }
+
 }
