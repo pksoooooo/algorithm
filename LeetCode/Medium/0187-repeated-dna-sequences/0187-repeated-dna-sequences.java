8ee22b8c9fd56ec40008f8dfea7cf14abaa5fea2
Kwonsoo Park <45193994+pksoooooo@users.noreply.github.com>
2025-04-21 15:46:18 +0900

    Time: 20 ms (49.86%), Space: 53.9 MB (13.49%) - LeetHub

--- LeetCode/Medium/0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
+++ LeetCode/Medium/0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
@@ -1,18 +1,17 @@
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-
-        ArrayList<String> resultList = new ArrayList<>();
-        Set<String> tempSet = new HashSet<>();
+        List<String> list = new ArrayList<>();
+        Set<String> set = new HashSet<>();
         int length = s.length();
 
-        for(int i = 0; i <= length - 10; i++) {
+        for(int i = 0; i <= length - 10; i++ ){
             String str = s.substring(i, i + 10);
-            if(!tempSet.add(str) && !resultList.contains(str)){
-                resultList.add(str);
+            if(!set.add(str) && !list.contains(str)){
+                list.add(str);
             }
         }
 
-        return resultList;
+        return list;
         
     }
 }
