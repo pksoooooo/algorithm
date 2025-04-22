cbd08f103728cd94ff5989b422f9f8d3db182bd8
Kwonsoo Park <45193994+pksoooooo@users.noreply.github.com>
2025-04-22 15:55:29 +0900

    Time: 8 ms (14.68%), Space: 44.6 MB (21.04%) - LeetHub

--- LeetCode/Easy/1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.java
+++ LeetCode/Easy/1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.java
@@ -2,6 +2,7 @@ class Solution {
     public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
 
         int count = 0;
+        Arrays.sort(arr2);
         for (int i = 0; i < arr1.length; i++) {
             boolean flag = false;
             for (int j = 0; j < arr2.length; j++) {
