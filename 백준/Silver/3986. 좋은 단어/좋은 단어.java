aeb3fb1f9e009108e62135fc7e99aa215a88a0af
Kwonsoo Park <45193994+pksoooooo@users.noreply.github.com>
2025-04-23 15:48:55 +0900

    [Silver IV] Title: 좋은 단어, Time: 360 ms, Memory: 23172 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.java
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.java
@@ -7,24 +7,20 @@ class Main {
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
-        
         int count = 0;
 
         for (int i = 0; i < N; i++) {
             String word = sc.next();
             Stack<Character> stack = new Stack<>();
-
             stack.push(word.charAt(0));
-
             for (int j = 1; j < word.length(); j++) {
-                char ch = word.charAt(j);
-                if (!stack.isEmpty() && stack.peek() == ch) {
+                if (!stack.isEmpty() && stack.peek() == word.charAt(j)) {
                     stack.pop();
                 } else {
-                    stack.push(ch);
+                    stack.push(word.charAt(j));
                 }
-            }
 
+            }
             if (stack.isEmpty()) {
                 count++;
             }
