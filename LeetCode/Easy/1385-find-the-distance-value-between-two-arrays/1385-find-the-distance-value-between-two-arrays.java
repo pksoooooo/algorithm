6c65c7466c44f4ee2077b78834bcc88576be537d
Kwonsoo Park <45193994+pksoooooo@users.noreply.github.com>
2025-04-23 16:46:21 +0900

    Time: 3 ms (96.53%), Space: 44.3 MB (57.24%) - LeetHub

--- LeetCode/Easy/1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.java
+++ LeetCode/Easy/1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.java
@@ -1,37 +1,37 @@
 class Solution {
     public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
 
-        int count = 0;
+        // if (arr2.length < arr1.length) {
+        //     return findTheDistanceValue(arr2, arr1, d);
+        // }
+
         Arrays.sort(arr2);
-        for (int i = 0; i < arr1.length; i++) {
-            if (!isWithinDistance(arr1[i], arr2, d)) {
+        int count = 0;
+        for (int num : arr1) {
+            if (!exist(num, arr2, d)) {
                 count++;
             }
-
         }
 
         return count;
-
     }
 
-    static boolean isWithinDistance(int n, int[] arr2, int d) {
+    static boolean exist(int num, int[] arr2, int d) {
+
         int left = 0;
         int right = arr2.length - 1;
 
         while (left <= right) {
             int mid = (left + right) / 2;
-
-            if (Math.abs(n - arr2[mid]) <= d) {
+            if (Math.abs(num - arr2[mid]) <= d) {
                 return true;
-            } else if (arr2[mid] < n) {
+            } else if (arr2[mid] < num) {
                 left = mid + 1;
             } else {
                 right = mid - 1;
             }
-
         }
 
         return false;
-
     }
 }
