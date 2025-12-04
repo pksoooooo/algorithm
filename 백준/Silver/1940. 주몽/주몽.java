cb5e76ec92e7f71f1c3fc89a5cb2e89f81499c9b
Kwonsoo Park <45193994+pksoooooo@users.noreply.github.com>
2025-12-04 10:40:33 +0900

    [Silver IV] Title: 주몽, Time: 320 ms, Memory: 30716 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -1,30 +1,41 @@
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.Arrays;
 
 class Main {
     public static void main(String[] args) throws IOException {
+
         Scanner sc = new Scanner(System.in);
+
         int N = sc.nextInt();
         int M = sc.nextInt();
-        int count = 0;
 
-        int[] arr = new int[N];
-
-        // 재료를 배열에 담는다
+        int[] A = new int[N];
 
         for (int i = 0; i < N; i++) {
-            arr[i] = sc.nextInt();
+            A[i] = sc.nextInt();
         }
 
-        // 두가지 조합에서 M이랑 같은 값을 찾아서 count를 증가 시킨다.
-        for (int i = 0; i < N; i++) {
-            for (int j = i + 1; j < N; j++) {
-                if (arr[i] + arr[j] == M) {
-                    count++;
-                }
+        Arrays.sort(A);
+
+        int count = 0;
+        int startIdx = 0;
+        int endIdx = N - 1;
+
+        while (startIdx < endIdx) {
+            int sum = A[startIdx] + A[endIdx];
+            if (sum == M) {
+                count++;
+                startIdx++;
+                endIdx--;
+            } else if (sum < M) {
+                startIdx++;
+            } else {
+                endIdx--;
             }
         }
 
         System.out.println(count);
+
     }
 }
