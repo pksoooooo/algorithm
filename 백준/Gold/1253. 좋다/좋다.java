4627b2697494fbbeba6726f9ad331ef584ce4349
Kwonsoo Park <45193994+pksoooooo@users.noreply.github.com>
2025-12-05 10:33:24 +0900

    [Gold IV] Title: 좋다, Time: 248 ms, Memory: 18752 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -9,40 +9,50 @@ class Main {
         int N = sc.nextInt();
         int[] A = new int[N];
 
+        // 배열에 담는다.
         for (int i = 0; i < N; i++) {
             A[i] = sc.nextInt();
         }
 
+        // 배열을 오름 차순 으로 정렬 한다.
         Arrays.sort(A);
-        // System.out.println(Arrays.toString(A));
+
         int count = 0;
 
-        for (int i = 0; i < N; i++) {
+        for (int targetIdx = 0; targetIdx < N; targetIdx++) {
 
-            int target = A[i];
+            // 타겟 값 기준으로 초기 변수 초기회
+            int target = A[targetIdx];
             int startIdx = 0;
             int endIdx = N - 1;
 
             while (startIdx < endIdx) {
+
+                // sum 변수 지정
                 int sum = A[startIdx] + A[endIdx];
-                if (target == sum) {
-                    if (startIdx != i && endIdx != i) {
+
+                if (sum == target) {
+                    // 찾을 idx와 tartget idx가 같지 않아야 한다.
+                    if (startIdx != targetIdx && endIdx != targetIdx) {
                         count++;
                         break;
-                    } else if (startIdx == i) {
+                        // 같다면 start up
+                    } else if (startIdx == targetIdx) {
                         startIdx++;
-                    } else if (endIdx == i) {
+                        // 같다면 end dowm
+                    } else if (endIdx == targetIdx) {
                         endIdx--;
                     }
-                } else if (sum < target) {
+
+                } else if (sum < target) { // sum이 작으면 startIdx up
                     startIdx++;
-                } else {
+                } else { // sum이 크면 endIdx down;
                     endIdx--;
                 }
             }
-
         }
 
         System.out.println(count);
+
     }
 }
